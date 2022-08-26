7cf33c8653c672361f12171a716bda363234d782
Raja Mukherji <rajamukherji@gmail.com>
2022-08-26 05:12:10 +0100

    Updates.

--- minicbor_writer.c
+++ minicbor_writer.c
@@ -33,39 +33,14 @@ static void MINICBOR(write_number)(MINICBOR_WRITE_PARAMS, uint64_t Absolute, uns
 	} else if (Absolute <= 0xFFFF) {
 		Bytes[0] = Base + 0x19;
 		*(uint16_t *)(Bytes + 1) = __builtin_bswap16(Absolute);
-		//Bytes[2] = Absolute & 0xFF;
-		//Absolute >>= 8;
-		//Bytes[1] = Absolute & 0xFF;
 		MINICBOR(write)(MINICBOR_WRITE_ARGS, Bytes, 3);
 	} else if (Absolute <= 0xFFFFFFFF){
 		Bytes[0] = Base + 0x1A;
 		*(uint32_t *)(Bytes + 1) = __builtin_bswap32(Absolute);
-		//Bytes[4] = Absolute & 0xFF;
-		//Absolute >>= 8;
-		//Bytes[3] = Absolute & 0xFF;
-		//Absolute >>= 8;
-		//Bytes[2] = Absolute & 0xFF;
-		//Absolute >>= 8;
-		//Bytes[1] = Absolute & 0xFF;
 		MINICBOR(write)(MINICBOR_WRITE_ARGS, Bytes, 5);
 	} else {
 		Bytes[0] = Base + 0x1B;
 		*(uint64_t *)(Bytes + 1) = __builtin_bswap64(Absolute);
-		//Bytes[8] = Absolute & 0xFF;
-		//Absolute >>= 8;
-		//Bytes[7] = Absolute & 0xFF;
-		//Absolute >>= 8;
-		//Bytes[6] = Absolute & 0xFF;
-		//Absolute >>= 8;
-		//Bytes[5] = Absolute & 0xFF;
-		//Absolute >>= 8;
-		//Bytes[4] = Absolute & 0xFF;
-		//Absolute >>= 8;
-		//Bytes[3] = Absolute & 0xFF;
-		//Absolute >>= 8;
-		//Bytes[2] = Absolute & 0xFF;
-		//Absolute >>= 8;
-		//Bytes[1] = Absolute & 0xFF;
 		MINICBOR(write)(MINICBOR_WRITE_ARGS, Bytes, 9);
 	}
 }
